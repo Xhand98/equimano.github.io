65263160c638a6c172cbe814a431273df0016446
Hendrick <78098509+Xhand98@users.noreply.github.com>
2024-08-28 21:48:45 -0400

    idk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,23 @@
-import { defineConfig } from "astro/config"
-
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config";
+import sanity from 'astro-sanity';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: "https://xhand98.github.io/equimano.github.io/",
-})
+  build: {
+    format: "directory",
+    assetsPrefix: "https://xhand98.github.io/equimano.github.io/",
+  },
+  server: {
+    tailwindConfig: './tailwind.config.js',
+  },
+  integrations: [
+    tailwind(),
+    sanity({
+      projectId: 'kkdxagg1',
+      dataset: 'production',
+      apiVersion: '2023-05-01',
+      useCdn: true,
+    }),
+  ]
+});
